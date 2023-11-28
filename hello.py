44b230f8d920e9998aecce84189eed43b892fa6a
AmeenKM27 <amsn27km@gmail.com>
2023-11-28 23:41:59 +0530

    second commit

--- hello.py
+++ hello.py
@@ -1,7 +1,15 @@
 from flask import Flask, request, jsonify, render_template
 from PIL import Image
-import io
+from io import BytesIO
+import requests
 import base64
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+# Initialize Firebase Admin SDK
+cred = credentials.Certificate("key.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 app = Flask(__name__)
 
@@ -9,70 +17,88 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/merge_images', methods=['POST'])
-def merge_images():
-    if 'background' not in request.files or 'overlay' not in request.files:
+
+
+# @app.route('/create/<int:id>', methods=['POST'])
+@app.route('/create/<string:user_id>')
+
+    # if user.exists:
+    #     user_data = user.to_dict()
+    #     return render_template('user_template.html', user_data=user_data)
+    # else:
+    #     return f"No user found with ID: {user_id}", 404
+
+def create(user_id):
+    users_ref = db.collection('users').document(user_id)
+    user = users_ref.get()
+    if user.exists:
+        
+            
+        # user_data = []
+        # for doc in docs:
+        #     user_data.append(doc.to_dict())
+        user_data = user.to_dict()
+        frame_image=user_data['user_image']
+        client_title=user_data['user_title']
+
+        return render_template("create.html",user_image=frame_image,user_title=client_title,user_id=user_id)
+    
+@app.route('/create/<string:user_id>/download',methods=['POST'])
+def image_rendering(user_id):
+    users_ref = db.collection('users').document(user_id)
+    user = users_ref.get()
+    if user.exists:
+        
+            
+        # user_data = []
+        # for doc in docs:
+        #     user_data.append(doc.to_dict())
+        user_data = user.to_dict()
+        frame_image=user_data['user_image']
+    if 'user_image' not in request.files:
         return jsonify({'error': 'Missing input files'}), 400
 
-    img1 = request.files['background']
-    img2 = request.files['overlay']
+    img1 = request.files['user_image']
     
     from PIL import Image, ImageDraw, ImageOps
 
-    # Open your images
-    fp = Image.open(img1)
-    fc = Image.open(img2)
+        # Open your images
+    fc = Image.open(img1)
+    response = requests.get(frame_image)
+    if response.status_code == 200:
+        with open('img2', 'wb') as file:
+            file.write(response.content)
+    
+    fp = Image.open('img2')
 
-    # Resize the second image
+        # Resize the second image
     r = fc.resize((150, 200))
 
-    # Create a new image for the mask
+        # Create a new image for the mask
     mask = Image.new('L', r.size, 0)
 
-    # Create a draw object
+        # Create a draw object
     draw = ImageDraw.Draw(mask)
 
-    # Draw a white ellipse on the mask
+        # Draw a white ellipse on the mask
     draw.ellipse((0, 0, 150, 200), fill=255)
 
-    # Create a composite image using the resized image and the mask
+        # Create a composite image using the resized image and the mask
     result = ImageOps.fit(r, mask.size, method=0, bleed=0.0, centering=(0.5, 0.5))
     result.putalpha(mask)
-    
-    # Paste the result image onto the first image
+        
+        # Paste the result image onto the first image
     fp.paste(result, (200, 150), result)
+    buffered = BytesIO()
+    fp.save(buffered, format="PNG")
+    image_url = base64.b64encode(buffered.getvalue()).decode('utf-8')
+        
+        # Save the merged image as 'merged_image.png'
 
-    # fp.show()
+        # Read the saved image file and encode it as base64
 
-    # Ensure the files are PNG images
-    # if fp.mimetype != 'image/png' or fc.mimetype != 'image/png':
-    #     return jsonify({'error': 'Please provide PNG images only'}), 400
 
-    # background = Image.open(fp)
-    # overlay = Image.open(fc)
-
-    # # Resize overlay image to fit within the background
-    # overlay = overlay.resize((200, 200))  # Adjust the size as needed
-
-    # # Paste the overlay image onto the background
-    # background.paste(overlay, (50, 50))  # Adjust the position as needed
-
-    # Save the modified image to a BytesIO object
-    # output = io.BytesIO()
-    # fp.save(output, format='PNG')
-    # output.seek(0)
-
-    # # Encode the image as base64
-    # encoded_img = base64.b64encode(output.read()).decode('utf-8')
-
-    # return jsonify({'image': encoded_img})
-    fp.save('merged_image.png')  # Save the merged image as 'merged_image.png'
-
-    # Read the saved image file and encode it as base64
-    with open('merged_image.png', 'rb') as image_file:
-        encoded_img = base64.b64encode(image_file.read()).decode('utf-8')
-
-    return jsonify({'image': encoded_img})
+    return render_template('result.html',image_url=image_url)
 
 if __name__ == '__main__':
     app.run(debug=True)
