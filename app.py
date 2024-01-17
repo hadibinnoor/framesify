66c0b08dbe9e9007cb36ce0af169c7a20b0bcff6
Shiyas-N <muhammedshiyasn811@gmail.com>
2024-01-17 17:33:56 +0530

    Error Fix

--- app.py
+++ app.py
@@ -57,20 +57,20 @@ def image_rendering(user_id):
         expiration_time = datetime.timedelta(minutes=60)
         image_url = blob.generate_signed_url(expiration_time)
 
-        qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
-        border=4,
-        )
-        qr.add_data(image_url)
-        qr.make(fit=True)
-        qr_img = qr.make_image(fill_color="black", back_color="white")
-        buffer = BytesIO()
-        qr_img.save(buffer, format="PNG")
-        image_data = base64.b64encode(buffer.getvalue()).decode("utf-8")
-        mime_type="data:image/png;base64,"
-        qr_code=f'{mime_type}{image_data}'
+        # qr = qrcode.QRCode(
+        # version=1,
+        # error_correction=qrcode.constants.ERROR_CORRECT_L,
+        # box_size=10,
+        # border=4,
+        # )
+        # qr.add_data(image_url)
+        # qr.make(fit=True)
+        # qr_img = qr.make_image(fill_color="black", back_color="white")
+        # buffer = BytesIO()
+        # qr_img.save(buffer, format="PNG")
+        # image_data = base64.b64encode(buffer.getvalue()).decode("utf-8")
+        # mime_type="data:image/png;base64,"
+        # qr_code=f'{mime_type}{image_data}'
 
         # Create a new document in Firestore with the user ID and image URL
         doc_ref = db.collection(f'users/{user_id}/user_images').document()
@@ -78,13 +78,13 @@ def image_rendering(user_id):
             'user_id': user_id_fb,
             'image_url': image_url,
         })
-        if user_id=="P6hGi":
-            return jsonify(qr_code)
-        else:
-             return jsonify(image_details['mime_image'])
+        # if user_id=="P6hGi":
+        #     return jsonify(qr_code)
+        # else:
+        return jsonify(image_details['mime_image'])
     else:
     # Handle the error accordingly, e.g., return an error response
-            return jsonify({'error': 'Image processing failed'})
+        return jsonify({'error': 'Image processing failed'})
 
 # @app.route('/campaign/<string:user_id>/qr',methods=['POST'])
 # def qr_code_generation(user_id):
