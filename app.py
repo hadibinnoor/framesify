6bfaa1e4c30bbdda9eecb654264c68c0389e388e
AmeenKM27 <amsn27km@gmail.com>
2024-01-17 20:43:29 +0530

    fix

--- app.py
+++ app.py
@@ -68,46 +68,46 @@ def image_rendering(user_id):
     # Handle the error accordingly, e.g., return an error response
             return jsonify({'error': 'Image processing failed'})
 
-@app.route('/campaign/<string:user_id>/qr',methods=['POST'])
-def qr_code_generation(user_id):
-    user_details=get_user_details(user_id,db)
-    if user_details:
-        image_details=image_frame_rendering(user_details)
-        image_data=image_details['base64_image']
-        image_data = re.sub('^data:image/.+;base64,', '', image_data)
-        # Decode the base64 image data
-        image_bytes = base64.b64decode(image_data)
-        user_id_fb = str(uuid.uuid4())
+# @app.route('/campaign/<string:user_id>/qr',methods=['POST'])
+# def qr_code_generation(user_id):
+#     user_details=get_user_details(user_id,db)
+#     if user_details:
+#         image_details=image_frame_rendering(user_details)
+#         image_data=image_details['base64_image']
+#         image_data = re.sub('^data:image/.+;base64,', '', image_data)
+#         # Decode the base64 image data
+#         image_bytes = base64.b64decode(image_data)
+#         user_id_fb = str(uuid.uuid4())
 
-        # Save the image to Firebase Storage
-        filename = f'{user_id_fb}.jpg'  # Adjust the file extension as needed
-        blob = bucket.blob(filename)
-        blob.upload_from_string(image_bytes, content_type='image/jpeg')  # Adjust the MIME type as needed
+#         # Save the image to Firebase Storage
+#         filename = f'{user_id_fb}.jpg'  # Adjust the file extension as needed
+#         blob = bucket.blob(filename)
+#         blob.upload_from_string(image_bytes, content_type='image/jpeg')  # Adjust the MIME type as needed
 
-        # Get the URL of the uploaded image
-        # Set the expiration time for the URL
-        expiration_time = datetime.timedelta(minutes=60)
-        image_url = blob.generate_signed_url(expiration_time)
-        qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
-        border=4,
-        )
-        qr.add_data(image_url)
-        qr.make(fit=True)
-        qr_img = qr.make_image(fill_color="black", back_color="white")
-        # Create a new document in Firestore with the user ID and image URL
-        doc_ref = db.collection(f'users/{user_id}/user_images').document()
-        doc_ref.set({
-            'user_id': user_id_fb,
-            'image_url': image_url,
-        })
+#         # Get the URL of the uploaded image
+#         # Set the expiration time for the URL
+#         expiration_time = datetime.timedelta(minutes=60)
+#         image_url = blob.generate_signed_url(expiration_time)
+#         qr = qrcode.QRCode(
+#         version=1,
+#         error_correction=qrcode.constants.ERROR_CORRECT_L,
+#         box_size=10,
+#         border=4,
+#         )
+#         qr.add_data(image_url)
+#         qr.make(fit=True)
+#         qr_img = qr.make_image(fill_color="black", back_color="white")
+#         # Create a new document in Firestore with the user ID and image URL
+#         doc_ref = db.collection(f'users/{user_id}/user_images').document()
+#         doc_ref.set({
+#             'user_id': user_id_fb,
+#             'image_url': image_url,
+#         })
 
-        return jsonify(qr_img)
-    else:
-    # Handle the error accordingly, e.g., return an error response
-            return jsonify({'error': 'Image processing failed'})
+#         return jsonify(qr_img)
+#     else:
+#     # Handle the error accordingly, e.g., return an error response
+#             return jsonify({'error': 'Image processing failed'})
 
 if __name__ == '__main__':
     app.run(debug=True)
