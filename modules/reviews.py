c3893d3f57d9a6994652b0b705b4a242cc469f70
AmeenKM27 <amsn27km@gmail.com>
2024-06-08 19:10:48 +0530

    review route

--- modules/reviews.py
+++ modules/reviews.py
@@ -16,6 +16,7 @@ def get_user_details(user_id,db):
             edit_frame_data = response.content
             edit_frame_np = np.frombuffer(edit_frame_data, np.uint8)
             edit_frame = cv2.imdecode(edit_frame_np, cv2.IMREAD_COLOR)
+            text=user_data['text_field']
             text_coordinates=None
             if 'text_coordinates' in user_data.keys():
                   text_coordinates=user_data['text_coordinates']
@@ -29,15 +30,17 @@ def get_user_details(user_id,db):
             else:
                   text_size=40
             client_title=user_data['user_title']
-            return {'client_title':client_title,'edit_frame':edit_frame,'text_coordinates':text_coordinates,'text_color':text_color,'text_size':text_size}
+            return {'client_title':client_title,'edit_frame':edit_frame,'text_coordinates':text_coordinates,'text_color':text_color,'text_size':text_size,'text':text}
 
 def text_placing(user_details,data,image=None):
         if image is None:
                 image=user_details['edit_frame']
         text_field=user_details['text']
         font_color=tuple(user_details['text_color'])
+        print(text_field)
         if data!=None:
                 text_coordinates=user_details['text_coordinates']
+                print(text_coordinates)
                 text_coordinates_dict={}
         # Create an array and append the parts
 
@@ -64,11 +67,11 @@ def text_placing(user_details,data,image=None):
                 width,height,_=image.shape
                 # center_x,center_y=int(width*0.347),int(height*1.10)
 
-                x_coordinates=[]
-                y_coordinates=[]
+                x_coordinates={}
+                y_coordinates={}
                 for key in data.keys():
-                        x_coordinates.append(int(text_coordinates_dict[key][0]*width))
-                        y_coordinates.append(int(text_coordinates_dict[key][1]*height))
+                        x_coordinates[key]=int(text_coordinates_dict[key][0]*width)
+                        y_coordinates[key]=int(text_coordinates_dict[key][1]*height)
                
                 text_positions={}
 
@@ -92,7 +95,7 @@ def text_placing(user_details,data,image=None):
                 # Prepend the MIME type to the base64 encoded image data
                 result_base64_with_mime = f"{mime_type}{result_base64}"
                 
-                return {'mime_image':result_base64_with_mime,'base64_image':result_base64}
+                return {'mime_image':result_base64_with_mime}
 def review_handler(user_id,db,data):
         user_details=get_user_details(user_id,db)
         image_details=text_placing(user_details,data,image=user_details['edit_frame'])
